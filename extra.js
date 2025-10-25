dfdada8052afc77dabdee0267ab62839ae8eb2f8
melissavanbussel <melissalisevb@gmail.com>
2025-10-25 23:12:57 +0000

    Deploying to gh-pages from @ melissavanbussel/rlo2025@47a6778e073e00945a8beb854d83d2664ec2b968 🚀

--- extra.js
+++ extra.js
@@ -7,16 +7,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const brand = document.querySelector(".navbar-brand");
-  if (!brand) return;
-
-  const isFileProtocol = window.location.protocol === "file:";
-  const imgSrc = isFileProtocol ? "../".repeat(7) + "logo.png" : "logo.png";
-
-  brand.innerHTML = `
-    <img src="${imgSrc}" height="30" style="vertical-align: middle; margin-right: 10px;">
-    <span style="vertical-align: middle;">rlo2025</span>
-  `;
-});
